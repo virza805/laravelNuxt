1064d00e134196b249f9c10469dc0f9c508c6a4f
Tanvir Hasan <47018365+virza805@users.noreply.github.com>
2022-02-14 02:30:27 +0600

    done c3 todo app

---
+++ tailwind.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+# $ node -v => for check install node
+# npm init -y => 
+# npm i -D tailwindcss => 
+# npx tailwindcss init => 
+# npm run build => 
+#  => 
+#  => 
+#  => 
+#  => 
+#  => 
+#  => 
+#  => 
+
+
+
+
+
