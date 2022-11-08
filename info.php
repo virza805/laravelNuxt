7776c9aa51959b46d113e6054904732b5d854a44
Tanvir Md. Al-Amin <52990610+1mdalamin1@users.noreply.github.com>
2022-11-08 18:12:03 +0600

    again setup in WebOneDevs

---
+++ info.php
@@ -0,0 +1,31 @@
+<?php 
+/*
+=========== >>>> = Backend (php artisan serve) = <<<<============
+composer create-project laravel/laravel:^8.0 backendLaravel
+
+
+# php artisan migrate =>
+# php artisan passport:install =>{
+    Personal access client created successfully.
+    Client ID: 970dbb74-46dc-429a-8a26-9b3ec3f9596b
+    Client secret: oBq0DuET1RgamhLS0z06bljtA0bOUClkJwr2MQQk
+    Password grant client created successfully.
+    Client ID: 970dbb74-618f-448d-8381-00bd985e7e7d
+    Client secret: evU8aMfEYI1jEgL2drg6j4BPS0zAfcd7PUNWt0FJ
+    }php artisan passport:keys => if download github file then run this || php artisan passport:client --personal
+
+
+============ >>>> = Frontend || Client Site vueNuxt (npm run dev) = <<<<============
+
+
+
+
+
+
+
+
+
+
+
+
+ */
